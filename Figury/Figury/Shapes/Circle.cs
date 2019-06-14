836df8c21aaadc4e725ca8e1867b2073e0e8d388
Kawwolski <47087721+Kawwolski@users.noreply.github.com>
2019-06-14 15:35:22 +0200

    Add files via upload

---
+++ Figury/Figury/Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Figury.Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle()
+        {
+            pointList = new List<Point>();
+        }
+
+        override public double Area()
+        {
+            double area;
+            double length = Math.Sqrt(Math.Pow(pointList[1].X - pointList[0].X, 2) + Math.Pow(pointList[1].Y - pointList[0].Y, 2));
+            return area = Math.PI * Math.Pow(length, 2);
+        }
+
+        override public double Circumference()
+        {
+            double circumference;
+            double length = Math.Sqrt(Math.Pow(pointList[1].X - pointList[0].X, 2) + Math.Pow(pointList[1].Y - pointList[0].Y, 2));
+            return circumference = 2 * Math.PI * length;
+        }
+    }
+}
---
+++ Figury/Figury/Shapes/Shape.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Figury
+{
+    public abstract class Shape
+    {
+        public List<Point> pointList { get; set; }
+
+        public void OrderPoints()
+        {
+            if (pointList.Count > 2)
+            {
+                
+                double yMin = pointList.Min(o => o.Y);
+                List<Point> pp = pointList.FindAll(o => o.Y == yMin);
+                double xMin = pp.Min(o => o.X);
+
+                Point pMin = pointList.Find(o => o.X == xMin && o.Y == yMin);
+                pointList.Remove(pMin);
+
+                List<Point> wspolrzedne = new List<Point>();
+                List<KeyValuePair<int, double>> katy = new List<KeyValuePair<int, double>>();
+
+                for (int i = 0; i < pointList.Count; i++)
+                {
+                    Point p = new Point();
+                    p.X = pointList[i].X - pMin.X;
+                    p.Y = pointList[i].Y - pMin.Y;
+                    wspolrzedne.Add(p);
+                }
+
+                Vector vMin = new Vector(pMin.X + 1, 0);
+                for (int i = 0; i < wspolrzedne.Count; i++)
+                {
+                    Vector v = new Vector(wspolrzedne[i].X, wspolrzedne[i].Y);
+                    katy.Add(new KeyValuePair<int, double>(i, Vector.AngleBetween(vMin, v)));
+                }
+
+                List<KeyValuePair<int, double>> katyPosortowane = katy.OrderBy(o => o.Value).ToList();
+
+                List<Point> tmp = new List<Point>();
+
+                for (int i = 0; i < katyPosortowane.Count; i++)
+                {
+                    tmp.Add(pointList[katyPosortowane[i].Key]);
+                }
+
+                pointList.Clear();
+                pointList.Add(pMin);
+
+                foreach (Point p in tmp)
+                    pointList.Add(p);
+            }
+        }
+
+        public abstract double Area();
+
+        public abstract double Circumference();
+
+        public void Dispose()
+        {
+            pointList.Clear();
+        }
+    }
+}
